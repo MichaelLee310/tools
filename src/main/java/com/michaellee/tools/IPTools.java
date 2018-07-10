b88a28196fcef1d5b3f605d41a5ffe9f45e9a4cf
Michael Lee <lizhang@yixia.com>
2018-07-10 18:03:48 +0800

    新增IPTools

---
+++ src/main/java/com/michaellee/tools/IPTools.java
@@ -0,0 +1,43 @@
+package com.michaellee.tools;
+
+public class IPTools {
+
+    /**
+     * 将四段式IP地址转换成十进制整型IP
+     * @param strIP
+     * @return
+     */
+    public static long ipToLong(String strIP) {
+        long[] ip = new long[4];
+        int position1 = strIP.indexOf(".");
+        int position2 = strIP.indexOf(".", position1 + 1);
+        int position3 = strIP.indexOf(".", position2 + 1);
+        ip[0] = Long.parseLong(strIP.substring(0, position1));
+        ip[1] = Long.parseLong(strIP.substring(position1 + 1, position2));
+        ip[2] = Long.parseLong(strIP.substring(position2 + 1, position3));
+        ip[3] = Long.parseLong(strIP.substring(position3 + 1));
+        return (ip[0] << 24) + (ip[1] << 16) + (ip[2] << 8) + ip[3];
+    }
+
+    /**
+     * 将十进制整型IP转换成四段式IP地址
+     * @param longIp
+     * @return
+     */
+    public static String longToIP(long longIp){
+        StringBuffer sb = new StringBuffer("");
+        //直接右移24位
+        sb.append(String.valueOf((longIp >>> 24)));
+        sb.append(".");
+        //将高8位置0，然后右移16位
+        sb.append(String.valueOf((longIp & 0x00FFFFFF) >>> 16));
+        sb.append(".");
+        //将高16位置0，然后右移8位
+        sb.append(String.valueOf((longIp & 0x0000FFFF) >>> 8));
+        sb.append(".");
+        //将高24位置0
+        sb.append(String.valueOf((longIp & 0x000000FF)));
+        return sb.toString();
+    }
+
+}
