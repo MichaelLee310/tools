36eccc71b50c03c8605a06791937423a6b6f1106
Michael Lee <lizhang@yixia.com>
2018-07-10 17:53:53 +0800

    新增StrTools
    字符串工具类

---
+++ src/main/java/com/michaellee/tools/StrTools.java
@@ -0,0 +1,40 @@
+package com.michaellee.tools;
+
+public class StrTools {
+    /**
+     * 判断字符串是否为数字，支持整数、小数、正数、负数
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isStrNumeric(String str) {
+        if (str.length() <= 0) {
+            return false;
+        } else {
+            Boolean strResult = str.matches("-?[0-9]+.?[0-9]*");
+            return strResult;
+        }
+    }
+
+    /**
+     * 判断字符串是否为整数
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isStrInt(String str) {
+        if (str.length() <= 0) {
+            return false;
+        } else {
+            //如果str的值超过int的最大值，则抛出异常。
+            try {
+                Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            Boolean strResult = str.matches("-?[0-9]*");
+            return strResult;
+        }
+    }
+}
